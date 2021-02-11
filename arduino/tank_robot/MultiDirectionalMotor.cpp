cbc6b8abfe12e89e6d87e402b466c9480dfc7e23
smiousse <stephane.miousse@gmail.com>
2021-02-10 20:37:29 -0500

    Test 2

--- arduino/tank_robot/MultiDirectionalMotor.cpp
+++ arduino/tank_robot/MultiDirectionalMotor.cpp
@@ -3,6 +3,12 @@
 
 MultiDirectionalMotor::MultiDirectionalMotor(String name, int motorLeftCtrlPin, int motorRightCtrlPin, int motorLeftSpeedPin, int motorRightSpeedPin){
 
+  _currentSpeed = 200;
+  _turnOffset = 54;
+
+  _motorLeftOffset = 0;
+  _motorRightOffset = 5;
+
   _name = name;
   _motorLeftCtrlPin = motorLeftCtrlPin;
   _motorRightCtrlPin = motorRightCtrlPin;
@@ -20,22 +26,24 @@ void MultiDirectionalMotor::init(){
   pinMode(_motorLeftSpeedPin, OUTPUT);
 
   stop();
+  _calculateSpeed("normal", _motorRightOffset);
+  _calculateSpeed("normal", _motorLeftOffset);
 }
 
 void MultiDirectionalMotor::forward(){
   digitalWrite(_motorLeftCtrlPin,LOW);
   digitalWrite(_motorRightCtrlPin,LOW);
 
-  analogWrite(_motorRightSpeedPin,200);
-  analogWrite(_motorLeftSpeedPin,200);
+  analogWrite(_motorRightSpeedPin, _calculateSpeed("normal", _motorRightOffset));
+  analogWrite(_motorLeftSpeedPin, _calculateSpeed("normal", _motorLeftOffset));
 }
 
 void MultiDirectionalMotor::reverse(){
   digitalWrite(_motorLeftCtrlPin,HIGH);
   digitalWrite(_motorRightCtrlPin,HIGH);
 
-  analogWrite(_motorRightSpeedPin,200);
-  analogWrite(_motorLeftSpeedPin,200);
+  analogWrite(_motorRightSpeedPin, _calculateSpeed("normal", _motorRightOffset));
+  analogWrite(_motorLeftSpeedPin, _calculateSpeed("normal", _motorLeftOffset));
 }
 
 void MultiDirectionalMotor::fullLeft(){
@@ -43,48 +51,48 @@ void MultiDirectionalMotor::fullLeft(){
   digitalWrite(_motorLeftCtrlPin,HIGH);
   digitalWrite(_motorRightCtrlPin,LOW);
 
-  analogWrite(_motorRightSpeedPin,255);
-  analogWrite(_motorLeftSpeedPin,255);
+  analogWrite(_motorRightSpeedPin, _calculateSpeed("turn", _motorRightOffset));
+  analogWrite(_motorLeftSpeedPin, _calculateSpeed("turn", _motorLeftOffset));
 }
 
 void MultiDirectionalMotor::fullRight(){
   digitalWrite(_motorLeftCtrlPin,LOW);
   digitalWrite(_motorRightCtrlPin,HIGH);
 
-  analogWrite(_motorRightSpeedPin,255);
-  analogWrite(_motorLeftSpeedPin,255);
+  analogWrite(_motorRightSpeedPin, _calculateSpeed("turn", _motorRightOffset));
+  analogWrite(_motorLeftSpeedPin, _calculateSpeed("turn", _motorLeftOffset));
 }
 
 void MultiDirectionalMotor::forwardLeft(){
   digitalWrite(_motorLeftCtrlPin,LOW);
   digitalWrite(_motorRightCtrlPin,LOW);
 
-  analogWrite(_motorRightSpeedPin,255);
-  analogWrite(_motorLeftSpeedPin,200);
+  analogWrite(_motorRightSpeedPin, _calculateSpeed("turn", _motorRightOffset));
+  analogWrite(_motorLeftSpeedPin, _calculateSpeed("normal", _motorLeftOffset));
 }
 
 void MultiDirectionalMotor::forwardRight(){
   digitalWrite(_motorLeftCtrlPin,LOW);
   digitalWrite(_motorRightCtrlPin,LOW);
 
-  analogWrite(_motorRightSpeedPin,200);
-  analogWrite(_motorLeftSpeedPin,255);
+  analogWrite(_motorRightSpeedPin, _calculateSpeed("normal", _motorRightOffset));
+  analogWrite(_motorLeftSpeedPin, _calculateSpeed("turn", _motorLeftOffset));
 }
 
 void MultiDirectionalMotor::reverseLeft(){
   digitalWrite(_motorLeftCtrlPin,HIGH);
   digitalWrite(_motorRightCtrlPin,HIGH);
 
-  analogWrite(_motorRightSpeedPin,255);
-  analogWrite(_motorLeftSpeedPin,200);
+  analogWrite(_motorRightSpeedPin, _calculateSpeed("turn", _motorRightOffset));
+  analogWrite(_motorLeftSpeedPin, _calculateSpeed("normal", _motorLeftOffset));
 }
 
 void MultiDirectionalMotor::reverseRight(){
   digitalWrite(_motorLeftCtrlPin,HIGH);
   digitalWrite(_motorRightCtrlPin,HIGH);
 
-  analogWrite(_motorRightSpeedPin,200);
-  analogWrite(_motorLeftSpeedPin,255);
+  analogWrite(_motorRightSpeedPin, _calculateSpeed("normal", _motorRightOffset));
+  analogWrite(_motorLeftSpeedPin, _calculateSpeed("turn", _motorLeftOffset));
 }
 
 void MultiDirectionalMotor::stop(){
@@ -94,6 +102,45 @@ void MultiDirectionalMotor::stop(){
   analogWrite(_motorLeftSpeedPin,0);
 }
 
+void MultiDirectionalMotor::increaseSpeed(){
+  _currentSpeed+=20;
+  Serial.print("_currentSpeed = ");
+  Serial.println(_currentSpeed);
+}
+
+void MultiDirectionalMotor::decreaseSpeed(){
+  _currentSpeed-=20;
+  Serial.print("_currentSpeed = ");
+  Serial.println(_currentSpeed);
+}
+
+void MultiDirectionalMotor::resetSpeed(){
+  _currentSpeed = 200;
+  Serial.print("_currentSpeed = ");
+  Serial.println(_currentSpeed);
+}
+
 void MultiDirectionalMotor::_debug(String debugValue){
 
+}
+
+int MultiDirectionalMotor::_calculateSpeed(String actionType, int motorOffset){
+ 
+  int speed=_currentSpeed;
+  if(actionType == "turn"){
+    speed=(int)(_currentSpeed+motorOffset+_turnOffset);
+  } else if(actionType == "normal"){
+    speed=(int)(_currentSpeed+motorOffset);
+  }
+  if(speed > 255){
+    speed=255;
+  }
+  if(speed < 0){
+    speed=0;
+  }
+
+  Serial.print("speed = ");
+  Serial.println(speed);
+  
+  return speed;
 }
--- arduino/tank_robot/MultiDirectionalMotor.h
+++ arduino/tank_robot/MultiDirectionalMotor.h
@@ -18,14 +18,26 @@ class MultiDirectionalMotor
     void reverseRight();
     void stop();
     void init();
+    void increaseSpeed();
+    void decreaseSpeed();
+    void resetSpeed();
 
   private:
     String _name;
+
+    int _currentSpeed;
+    int _turnOffset;
+
+    int _motorLeftOffset;
+    int _motorRightOffset;
+
     int _motorLeftCtrlPin;
     int _motorRightCtrlPin;
     int _motorRightSpeedPin;
     int _motorLeftSpeedPin;
     void _debug(String debugValue);
+
+    int _calculateSpeed(String actionType, int motorOffset);
 };
 
 #endif
